b6915439483496ad72a0f38e20f06138f13daf8b
dmyerc <112904335+dmyerc@users.noreply.github.com>
2025-08-09 21:42:04 -0700

    uploaded skeleton

---
+++ Music Downloader.bat
@@ -0,0 +1,61 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+ECHO.
+ECHO Welcome to a very barebones audio downloader!
+ECHO This program accepts links from youtube, soundcloud, bandcamp and many other sites.
+ECHO.
+
+ECHO The full list of compatible streaming sites can be found at the link below:
+ECHO https://ytdl-org.github.io/youtube-dl/supportedsites.html
+ECHO.
+ECHO All output files are in .wav format
+ECHO.
+ECHO If you run into any errors, view the readme file. Chances r u need to update system variables, install c++ package, or install new version of ytdlp.exe
+
+:start
+ECHO.
+ECHO ======================================================================================================================
+ECHO.
+SET /P URL="[Enter URL] "
+ECHO.
+ECHO ======================================================================================================================
+goto formatList
+
+:formatList
+ECHO.
+yt-dlp --list-formats -F %URL%
+ECHO.
+ECHO ======================================================================================================================
+goto downloadSingle
+
+
+:downloadSingle
+ECHO.
+SET /P format="Paste ID of the version you wish to download: "
+ECHO.
+ECHO ======================================================================================================================
+ECHO.
+yt-dlp -f %format% --ffmpeg-location "ffmpeg/bin" --extract-audio --audio-format wav --audio-quality 0 -i --ignore-config --hls-prefer-native -o "MusicDownloads/%%(title)s.%%(ext)s" %URL%  
+ECHO.
+ECHO If successful, wav file(s) located in 'MusicDownloads' folder
+ECHO.
+ECHO ======================================================================================================================
+ECHO.
+
+SET /P response="[Finished? (enter y/n)] "
+
+if "!response!"=="n" (
+	goto start
+) else (
+	goto end
+)
+
+
+:end
+ECHO.
+ECHO ======================================================================================================================
+ECHO.
+ECHO Exiting Application
+PAUSE
+EXIT
+
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Video-Url-to-Audio-converter
+\# Video-Url-to-Audio-converter
+
 FFMPEG must be downloaded in order for ytdladvanced to run, youtube-dl.exe must also be added to the users system variables so it can be accessed by the bat 
+
 If a dll error appears on launch, the user must download c++ compatibility for a 64 bit system
+
